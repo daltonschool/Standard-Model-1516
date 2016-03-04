9fc78efccc400f5bc90dc345ef4395f6601d9360
higgs241 <nathanielostrer@gmail.com>
2016-03-04 16:28:00 -0500

    more pixy cam i2c code

--- YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/PixyCam.java
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/PixyCam.java
@@ -15,15 +15,49 @@ import org.swerverobotics.library.internal.II2cDevice;
 public class PixyCam {
 
 
+    I2cDeviceClient deviceClient;
+
 
     public PixyCam(OpMode context, II2cDevice ii2cDevice) {
         int i2cAddr8Bit = 127; // Just a guess, I think we set this from pixymon
         boolean closeOnOpModeStop = true;
 
-        I2cDeviceClient deviceClient = new I2cDeviceClient(context, ii2cDevice, i2cAddr8Bit, closeOnOpModeStop);
+        deviceClient = new I2cDeviceClient(context, ii2cDevice, i2cAddr8Bit, closeOnOpModeStop);
 
     }
 
+
+    private byte readByte() {
+        int ireg = 0;
+        return deviceClient.read8(ireg);
+    }
+
+    private void getStart() {
+        int ireg = 0;
+
+        while(readByte() != 0xaa55);
+        readByte(); //get the second part of the start
+    }
+
+    private int readTwoBytes() {
+        int twobytes = readByte() + (readByte() << 8);
+        return twobytes;
+    }
+
+    public void getObject() {
+        getStart();
+
+        int checksum = readTwoBytes(); //need to check to make sure checksum is proper
+
+        int signature = readTwoBytes();
+
+
+        int x = readTwoBytes();
+        int y = readTwoBytes();
+
+        int width = read
+
+    }
 
 
 
